fe26919e77e0e9547320e4c30b6faa9f306bfa9d
ShreyasGeetha <shreyasgeetha@gmail.com>
2021-12-21 21:08:03 +0530

    'cors'

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -3,13 +3,18 @@ import { HomeIcon, MenuIcon } from '@heroicons/react/solid'
 import { signOut, useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
 import IconsGroup from "../Header/IconsGroup";
+import { useRecoilState } from 'recoil';
+import { modalState } from '../../atoms/modalAtom';
 import Logo from "../Header/Logo";
 import Search from "../Header/Search";
+
 
 const Footer = () => {
 
   const { data: session } = useSession();
   const router = useRouter();
+
+  const [open, setOpen] = useRecoilState(modalState)
 
   const showHomePage = () => {
     router.push('/')
@@ -23,9 +28,9 @@ const Footer = () => {
           {
             session && 
               <>              
-                <PlusCircleIcon className="navBtn" />
+                <PlusCircleIcon onClick={() => setOpen(true)} className="navBtn" />
                 <HeartIcon className="navBtn"/>
-                <img onClick={signOut} src="https://hope-product-profile-images.s3.ap-southeast-1.amazonaws.com/kevinsystrom.jpg" alt="" className='h-10 w-10 cursor-pointer p-[2px] border rounded-full' />        
+                <img onClick={signOut} src={session.user.image} alt="" className='h-10 w-10 cursor-pointer p-[2px] border rounded-full' />        
               </>            
           }
         </div>
